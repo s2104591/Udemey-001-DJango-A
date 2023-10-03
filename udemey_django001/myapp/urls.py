816dfd022f698cd906955b9a1d5289dbd6f63305
marstab4 <marstab4@gmail.com>
2023-10-03 11:52:15 +1100

    11:52

--- udemey_django001/myapp/urls.py
+++ udemey_django001/myapp/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 
+
+
 urlpatterns=[
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path("<int:no1>/<int:no2>",views.functparam,name="add"),
+    path("<str:the_topic>",views.newsarticle,name="news"),
+
+
 ]
--- udemey_django001/myapp/views.py
+++ udemey_django001/myapp/views.py
@@ -4,8 +4,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+topics={"sports": "the sport-page", "arts": "the arts-page"}
+
+
 def index(request):
     return HttpResponse("Hello Mariano Rico, Udemey course start October 2023 " )
+
+
+
+def functparam(request, no1,no2):
+    sum=no1+no1
+    result=f' {no1} + {no2} = {sum}'
+    return HttpResponse(result)    
+
+def newsarticle(request, the_topic):
+    result= topics[the_topic]
+    return HttpResponse(result) 
+
     
 
 
