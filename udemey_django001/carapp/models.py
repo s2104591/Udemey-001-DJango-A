15732786e298b651bd8e66ec23bb694141ff19f2
marstab4 <marstab4@gmail.com>
2023-10-11 17:26:23 +1100

    1726

--- udemey_django001/carapp/models.py
+++ udemey_django001/carapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Car(models.Model):
+    model       =   models.CharField(max_length=50)
+    year        =   models.IntegerField(default=1900)
+    year        =   models.IntegerField(default=1900, validators=[ MinValueValidator(1900), MaxValueValidator(2100) ] )
+    price       =   models.FloatField(default=0)
+   
+
+
+
+    def __str__(self):
+        return f"id={self.id} model={self.model}, year={self.year}, price={self.price}"
+
+
+
+
+
--- udemey_django001/carapp/views.py
+++ udemey_django001/carapp/views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .import models as m
 
 # Create your views here.
 
 def addcar(request):
-    return render(request, "carapp/addcar.html")
+    if request.POST:
+        print("request made =",request.POST['model'] )
+        m.Car.objects.create(model=request.POST['model'])
+        print("car added")
+        return redirect (reverse('carapp:nm-listcars'))
+    else:    
+        return render(request, "carapp/addcar.html")
 
 
 
 def deletecar(request):
-    return render(request, "carapp/deletecar.html")
+    if request.POST:
+        id=request.POST['pk']
+        print("deleting car",id)
+        try:
+            m.Car.objects.get(pk=id).delete()
+            return redirect (reverse('carapp:nm-listcars'))
+        except:
+            print(id, "car not deleted")
+            return redirect (reverse('carapp:nm-listcars'))   
+
+        return render(request, "carapp/deletecar.html")
+
+    else:
+        return render(request, "carapp/deletecar.html")
     
 
 
 def listcars(request):
+    allcars= m.Car.objects.all();
+
+
     print("list- cars")
-    return render(request, "carapp/listcars.html")
+    return render(request, "carapp/listcars.html", context={'allcars':allcars})
