a1bad11c6b870d096c628f390c6bb364fa2f5167
marstab4 <marstab4@gmail.com>
2023-10-13 20:11:43 +1100

    Frid 20:11

--- udemey_django001/rentalcar/forms.py
+++ udemey_django001/rentalcar/forms.py
@@ -4,6 +4,8 @@ class ReviewForm(forms.Form):
     firstname=forms.CharField(max_length=30 )
     surname=forms.CharField(max_length=30 )
     email=forms.EmailField(label="whats your email")
-    review=forms.CharField(label="whats your review",max_length=30 )
+    review=forms.CharField(label="whats your review",max_length=30, 
+    widget=forms.Textarea( attrs={'class':'dashedred','rows':"4",'cols':'40'} ))
+
 
 
