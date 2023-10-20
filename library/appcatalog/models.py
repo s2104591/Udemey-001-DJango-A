127ca4295bcffa628050b777f8d2c72511b78869
marstab4 <marstab4@gmail.com>
2023-10-20 12:15:19 +1100

    12:15pm

--- library/appcatalog/models.py
+++ library/appcatalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    name= models.CharField(max_length=100)
+
+    def __str__(self) :
+        return self.name
+
+    pass
+
+class Book(models.Model):
+    title= models.CharField(max_length=100)
+    pass
+
+class Author(models.Model):
+    pass
+
+class BookInstance(models.Model):
+    pass
+
+class B(models.Model):
+    pass
+
+
