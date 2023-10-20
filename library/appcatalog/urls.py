5dab1f7beb7dde9a927982507c43d061b243bc52
marstab4 <marstab4@gmail.com>
2023-10-20 11:53:12 +1100

    Friday 11:53am

---
+++ library/appcatalog/urls.py
@@ -0,0 +1,8 @@
+# appcatalog
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='nm-index'),
+]
---
+++ library/appcatalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+#http://127.0.0.1:8000/
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello 002")
+    
+
