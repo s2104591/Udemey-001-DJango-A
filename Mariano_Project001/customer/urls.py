793f8b13886c8899f5e31d4bb4467354fd77adff
marstab4 <marstab4@gmail.com>
2023-10-23 21:19:11 +1100

    Mon 21:19

--- Mariano_Project001/customer/urls.py
+++ Mariano_Project001/customer/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path 
 from . import views 
 
+
 app_name = 'customer'
 
 
 urlpatterns = [
      path("one", views.one, name='nm-001'),
-     path("two", views.createcustomer, name='nm-createcustomer'),
-     path("three", views.thankyou, name='nm-thankyou'),
+     path("two", views.createcustomer, name='nm-createcustomer1'),
+     path("three", views.CustomerCreate.as_view(), name='nm-createcustomer2'),
+     path("four", views.thankyou, name='nm-thankyou'),
+
       
 ]
 
--- Mariano_Project001/customer/views.py
+++ Mariano_Project001/customer/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.urls import reverse
+from django.urls import reverse_lazy, reverse 
+
+from django.views.generic import CreateView,DetailView,ListView 
+
+
 
 from .forms import CustomerForm
 from .models import Customer
@@ -12,34 +16,53 @@ def one(request):
 def thankyou(request):
     return render(request, 'customer/thankyou.html') 
 
+def customer_exists(fname):
+    fname=fname.strip().lower()
+    count=Customer.objects.filter(firstname__iexact=fname).count()
+    return count>=1
 
+
+# method 1 of creating cutomer with validation - using Class Based Views 
+# note only need to use 1 method, Classed Based Views recommended
+class CustomerCreate(CreateView):
+    model= Customer
+    fields ="__all__"
+    success_url= reverse_lazy("customer:nm-thankyou")
+    template_name='customer/createcustomer2.html'
+    #failure_url = 'customer/sorry.html'
+
+    def form_valid(self,form):        
+        fname = form.cleaned_data['firstname']
+        okay=customer_exists(fname)==False
+
+        if okay:
+            return super().form_valid(form)
+        else:
+            #return super().form_invalid(form)
+            return self.render_to_response({'form': form,'message':f'{fname} already exists'})
+
+    pass
+
+
+
+
+# method 2 of creating cutomer with validation - using Model Views
+# note only need to use 1 method, Classed Based Views recommended . But Model Views still very good
 def createcustomer(request):
     if (request.method=='POST'):
         print("**  post request made")
         form = CustomerForm(request.POST) 
         message=""
         if form.is_valid():
-            
+                        
             fname=request.POST['firstname']
-            #request.POST['firstname']="changed name" # not allowed immutable
-            
-            
-            print("** post request, form YES valid, firstname=", fname)
-
-            count=Customer.objects.filter(firstname__iexact=fname).count()
-            print("count of customers with this firstname=",count)
-            
-            fname=fname.strip().lower()
-            count=Customer.objects.filter(firstname__iexact=fname).count()
-            
-            if count>=1:
-                message="name already exists"
-                return render (request,'customer/createcustomer.html', context= {'form':form,"message":message} )
-                pass
-
+            if customer_exists(fname):
+                message=f"{fname} already exists  - 002"
+                return render (request,'customer/createcustomer1.html', context= {'form':form,"message":message} )
             else:
                 newcust=Customer(firstname=fname )
-                print("new customer, firstname=", newcust.firstname, len(newcust.firstname) )
+                newcust.save()
+                print("new customer saved, firstname=", newcust.firstname, len(newcust.firstname) )
                 return redirect(reverse("customer:nm-thankyou" ))
 
             pass
@@ -51,8 +74,8 @@ def createcustomer(request):
     else:
         print("**  non post  Request made")
         form = CustomerForm()
-        return render (request,'customer/createcustomer.html', context= {'form':form,"message":"no message"} )
-        pass
+        return render (request,'customer/createcustomer1.html', context= {'form':form,"message":"no message"} )
+        
 
 
     
