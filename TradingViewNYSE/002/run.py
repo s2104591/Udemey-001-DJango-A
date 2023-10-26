1cf5a5810078a3d22016e62e2eeb69ee8435a528
marstab4 <marstab4@gmail.com>
2023-10-27 10:15:13 +1100

    10:15am

--- TradingViewNYSE/002/run.py
+++ TradingViewNYSE/002/run.py
@@ -1,13 +1,27 @@
-import NYSE_TradingTA as toolsTA
+import ETRADE_TradingTA as toolsTA
+
+doEtrade=True
+
+if doEtrade:
+    #toolsTA.get_Etrade_Individually(maxcount=300)
+
+    strresult,dfresult= toolsTA.Go(incEtrade=True, incNASDAQ=True, incNYSE=True, maxcount=40, savefile="Recommend-Oct27.csv",method=2)
+    print(strresult)
+    print(dfresult.head(20))
+
+
+else:
+
+    reccomend1=toolsTA.get_Summary_Single(symbol="RL", screener='america',exchange="NYSE")
+    reccomend2=toolsTA.get_Summary_Single(symbol="MIN", screener='australia',exchange="ASX")
+
+    strresult,dfresult= toolsTA.Go(incEtrade=False, incNASDAQ=True, incNYSE=True, maxcount=40, savefile="Recommend-Oct25.csv",method=2)
 
 
 
-reccomend=toolsTA.get_Summary_Single(symbol="RL", screener='america',exchange="NYSE")
-strresult,dfresult= toolsTA.Go(incNASDAQ=True, incNYSE=True, maxcount=40, savefile="Recommend-Oct25.csv",method=2)
-
-print(reccomend)
-print(strresult)
-print(dfresult.head(20))
+    print(reccomend1,"\n",reccomend2)
+    print(strresult)
+    print(dfresult.head(20))
 
 
 
