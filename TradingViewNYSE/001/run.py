490de4e3506b2eaeaac68f528adbbd30dd4e0f16
marstab4 <marstab4@gmail.com>
2023-10-28 20:40:07 +1100

    2040

--- TradingViewNYSE/001/run.py
+++ TradingViewNYSE/001/run.py
@@ -2,7 +2,7 @@ import NYSE_TradingTA as toolsTA
 
 
 
-reccomend=toolsTA.get_Summary_Single(symbol="RL", screener='america',exchange="NYSE")
+reccomend=toolsTA.get_Summary_Single(symbol="BANC", screener='america',exchange="NYSE")
 strresult,dfresult= toolsTA.Go(incNASDAQ=True, incNYSE=True, maxcount=40, savefile="Recommend-Oct25.csv",method=2)
 
 print(reccomend)
